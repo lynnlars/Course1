27382fcc31b2f37474568083293f9775b13a4d68
lynn larson <lynn.lars20@gmail.com>
2025-05-27 18:49:25 -0400

    modifying an elements text

---
+++ modifying/index.js
@@ -0,0 +1,7 @@
+// id = #header #body 
+
+//select h1 element and save it to a variable 
+
+var text = document.querySelector("#header").textContent 
+//change p text to be same as h1 
+document.querySelector("#body").textContent = text
