b3bcd279ac0e0ed41db79dd1ae85f2eb3933d578
lynn larson <lynn.lars20@gmail.com>
2025-05-27 20:59:54 -0400

    added modifying styles through javascript practice

--- modifying/index.js
+++ modifying/index.js
@@ -2,6 +2,19 @@
 
 //select h1 element and save it to a variable 
 
-var text = document.querySelector("#header").textContent 
+//var text = document.querySelector("#header").textContent 
 //change p text to be same as h1 
-document.querySelector("#body").textContent = text
+//document.querySelector("#body").textContent = text
+
+
+
+//modifying styles 
+
+var header = document.getElementById("header")
+console.log(typeof header.style) //typeof is to see the data type something is "what type of data is header.style" 
+
+header.style.color = "blue"
+
+//unlike CSS, styling in javascript requires camelCasing because the - is seen as a minus sign in JS
+
+document.body.style.backgroundColor = "pink"
