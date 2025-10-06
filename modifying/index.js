372314cfa70783e818b5446175759f4a0982fce4
lynn larson <lynn.lars20@gmail.com>
2025-10-06 16:56:53 -0400

    practice with modifying styles w classList and modifying the elements value in js. Also added css file

--- modifying/index.js
+++ modifying/index.js
@@ -10,11 +10,24 @@
 
 //modifying styles 
 
-var header = document.getElementById("header")
-console.log(typeof header.style) //typeof is to see the data type something is "what type of data is header.style" 
+// var header = document.getElementById("header")
+// console.log(typeof header.style) //typeof is to see the data type something is "what type of data is header.style" 
 
-header.style.color = "blue"
+// header.style.color = "blue"
 
-//unlike CSS, styling in javascript requires camelCasing because the - is seen as a minus sign in JS
+// //unlike CSS, styling in javascript requires camelCasing because the - is seen as a minus sign in JS
 
-document.body.style.backgroundColor = "pink"
+// document.body.style.backgroundColor = "pink"
+
+
+//modifying styles with classList 
+
+// document.querySelector("#header").classList.add("new-class")
+// console.log(document.querySelector("#header").className)
+
+
+//modifying elements value 
+
+var inputBox = document.getElementById("text-input")
+console.log(inputBox.value) //should be empty 
+inputBox.value = "shipping address" //used for when shipping and billing address the same, etc
